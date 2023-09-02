1a99aa256b208f700443acb09febd552a3854bd2
PatFagan <patrickfagan1586@gmail.com>
2023-09-02 09:17:39 -0400

    spin tree boy yourself!

--- FetchQuestGame/Assets/Scripts/UI/SpinOnClick.cs
+++ FetchQuestGame/Assets/Scripts/UI/SpinOnClick.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpinOnClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float initialClickPos;
+    float spinDist;
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(new Vector3(0f, .3f, 0f));
+        // ref: https://forum.unity.com/threads/solved-spin-a-3d-object-using-mouse.384167/
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialClickPos = Input.mousePosition.x;
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            spinDist += (Input.mousePosition.x - initialClickPos);
+            initialClickPos = Input.mousePosition.x;
+        }
+
+        // friction
+        if(spinDist > 0)
+        {
+            spinDist--;
+        }
+
+        gameObject.transform.Rotate(new Vector3(0f, -spinDist/200f, 0f));
+
     }
 }
